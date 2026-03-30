753fb91704203fc27c8fc896686a68f7a3ca9197
stasyzar <zarovskaanatasia@gmail.com>
2026-03-30 15:04:03 +0300

    feat(seed): add ratings and categories to shop products

--- server/seed.ts
+++ server/seed.ts
@@ -5,60 +5,66 @@ import { Shop } from './models/Shop.js';
 const initialShops = [
   {
     name: "Mc Donny",
+    rating: 5.0,
     products: [
-      { name: "Big Big Burger", price: 15, image: "https://picsum.photos/200/150?random=1" },
-      { name: "Small Big Burger", price: 10, image: "https://picsum.photos/200/150?random=2" },
-      { name: "Cheese Master", price: 12, image: "https://picsum.photos/200/150?random=13" },
-      { name: "French Fries", price: 5, image: "https://picsum.photos/200/150?random=14" },
-      { name: "Vanilla Milkshake", price: 8, image: "https://picsum.photos/200/150?random=20" }
+      { name: "Big Big Burger", category: "Burgers", price: 15, image: "https://picsum.photos/200/150?random=1" },
+      { name: "Small Big Burger", category: "Burgers",price: 10, image: "https://picsum.photos/200/150?random=2" },
+      { name: "Cheese Master", category: "Burgers",price: 12, image: "https://picsum.photos/200/150?random=13" },
+      { name: "French Fries", category: "Sides",price: 5, image: "https://picsum.photos/200/150?random=14" },
+      { name: "Vanilla Milkshake", category: "Burgers",price: 8, image: "https://picsum.photos/200/150?random=20" }
     ]
   },
   {
     name: "CFK",
+    rating: 4.5,
     products: [
-      { name: "Chicken Wings", price: 14, image: "https://picsum.photos/200/150?random=3" },
-      { name: "Hot Bucket", price: 22, image: "https://picsum.photos/200/150?random=4" },
-      { name: "Zinger Burger", price: 13, image: "https://picsum.photos/200/150?random=21" },
-      { name: "Chicken Strips", price: 11, image: "https://picsum.photos/200/150?random=22" }
+      { name: "Chicken Wings", category: "Sides",price: 14, image: "https://picsum.photos/200/150?random=3" },
+      { name: "Hot Bucket", category: "Sides",price: 22, image: "https://picsum.photos/200/150?random=4" },
+      { name: "Zinger Burger", category: "Burgers",price: 13, image: "https://picsum.photos/200/150?random=21" },
+      { name: "Chicken Strips", category: "Sides",price: 11, image: "https://picsum.photos/200/150?random=22" }
     ]
   },
   {
     name: "Pizza Port",
+    rating: 4.0,
     products: [
-      { name: "Pepperoni Pizza", price: 18, image: "https://picsum.photos/200/150?random=5" },
-      { name: "Margarita Pizza", price: 16, image: "https://picsum.photos/200/150?random=6" },
-      { name: "Four Cheese", price: 20, image: "https://picsum.photos/200/150?random=18" },
-      { name: "BBQ Chicken", price: 19, image: "https://picsum.photos/200/150?random=19" },
-      { name: "Hawaiian Pizza", price: 17, image: "https://picsum.photos/200/150?random=23" }
+      { name: "Pepperoni Pizza", category: "Pizzas",price: 18, image: "https://picsum.photos/200/150?random=5" },
+      { name: "Margarita Pizza", category: "Pizzas",price: 16, image: "https://picsum.photos/200/150?random=6" },
+      { name: "Four Cheese", category: "Pizzas",price: 20, image: "https://picsum.photos/200/150?random=18" },
+      { name: "BBQ Chicken", category: "Pizzas",price: 19, image: "https://picsum.photos/200/150?random=19" },
+      { name: "Hawaiian Pizza", category: "Pizzas",price: 17, image: "https://picsum.photos/200/150?random=23" }
     ]
   },
   {
     name: "Sushi Master",
+    rating: 3.5,
     products: [
-      { name: "California Roll", price: 12, image: "https://picsum.photos/200/150?random=7" },
-      { name: "Philadelphia Roll", price: 14, image: "https://picsum.photos/200/150?random=8" },
-      { name: "Dragon Roll", price: 18, image: "https://picsum.photos/200/150?random=15" },
-      { name: "Miso Soup", price: 7, image: "https://picsum.photos/200/150?random=16" },
-      { name: "Tempura Shrimp", price: 11, image: "https://picsum.photos/200/150?random=17" }
+      { name: "California Roll", category: "Rolls",price: 12, image: "https://picsum.photos/200/150?random=7" },
+      { name: "Philadelphia Roll", category: "Rolls",price: 14, image: "https://picsum.photos/200/150?random=8" },
+      { name: "Dragon Roll", category: "Rolls",price: 18, image: "https://picsum.photos/200/150?random=15" },
+      { name: "Miso Soup", category: "Soups",price: 7, image: "https://picsum.photos/200/150?random=16" },
+      { name: "Tempura Shrimp", category: "Soups",price: 11, image: "https://picsum.photos/200/150?random=17" }
     ]
   },
   {
     name: "Salad Bar",
+    rating: 2.5,
     products: [
-      { name: "Caesar Salad", price: 9, image: "https://picsum.photos/200/150?random=9" },
-      { name: "Greek Salad", price: 8, image: "https://picsum.photos/200/150?random=10" },
-      { name: "Tuna Salad", price: 11, image: "https://picsum.photos/200/150?random=24" },
-      { name: "Fruit Salad", price: 7, image: "https://picsum.photos/200/150?random=25" }
+      { name: "Caesar Salad", category: "Salads",price: 9, image: "https://picsum.photos/200/150?random=9" },
+      { name: "Greek Salad", category: "Salads",price: 8, image: "https://picsum.photos/200/150?random=10" },
+      { name: "Tuna Salad", category: "Salads",price: 11, image: "https://picsum.photos/200/150?random=24" },
+      { name: "Fruit Salad", category: "Salads",price: 7, image: "https://picsum.photos/200/150?random=25" }
     ]
   },
   {
     name: "Wok & Go",
+    rating: 4.0,
     products: [
-      { name: "Rice with Chicken", price: 11, image: "https://picsum.photos/200/150?random=11" },
-      { name: "Pad Thai", price: 13, image: "https://picsum.photos/200/150?random=12" },
-      { name: "Egg Noodles with Beef", price: 14, image: "https://picsum.photos/200/150?random=26" },
-      { name: "Spring Rolls", price: 6, image: "https://picsum.photos/200/150?random=27" },
-      { name: "Udon with Veggies", price: 12, image: "https://picsum.photos/200/150?random=28" }
+      { name: "Rice with Chicken", category: "Sides",price: 11, image: "https://picsum.photos/200/150?random=11" },
+      { name: "Pad Thai", category: "Sides",price: 13, image: "https://picsum.photos/200/150?random=12" },
+      { name: "Egg Noodles with Beef", category: "Sides", price: 14, image: "https://picsum.photos/200/150?random=26" },
+      { name: "Spring Rolls", category: "Rolls", price: 6, image: "https://picsum.photos/200/150?random=27" },
+      { name: "Udon with Veggies", category: "Soups", price: 12, image: "https://picsum.photos/200/150?random=28" }
     ]
   }
 ];
