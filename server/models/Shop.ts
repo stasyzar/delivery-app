69ab4742ef18380b2eeca6f63b88b1541ef77755
stasyzar <zarovskaanatasia@gmail.com>
2026-03-31 10:01:21 +0300

    fix(types): add category and rating fields to Product and Shop schemas

--- server/models/Shop.ts
+++ server/models/Shop.ts
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  category: {type: String, required: true},
   price: { type: Number, required: true },
   image: { type: String, required: true }
 });
 
 const ShopSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  rating: {type: Number, required: true},
   products: [ProductSchema]
 });
 
